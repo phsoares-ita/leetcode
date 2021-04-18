acb0096222f346b2cd42d1c725af5d87d5dcae64
Paulo Soares <phsoares.ita@gmail.com>
2021-04-18 15:39:49 -0300

    add two numbers refactor

--- spec/2_add_two_numbers_spec.rb
+++ spec/2_add_two_numbers_spec.rb
@@ -4,72 +4,62 @@ require 'byebug'
 class ListNode
   attr_accessor :val, :next
 
-  def initialize(val, _next = nil)
+  def initialize(val = 0, _next = nil)
     @val = val
     @next = _next
   end
 end
 
 def add_two_numbers(l1, l2)
-  head = nil
-  last_node = nil
-  carry = 0
-  while l1 || l2 || carry > 0
-    digit, carry = sum_values(l1, l2, carry)
-    node = ListNode.new(digit)
-    head ||= node
-    last_node.next = node if last_node
-    last_node = node
-    l1 = l1&.next
-    l2 = l2&.next
-  end
-  head
-end
+    carry = 0
+    head = nil
+    last_node = nil
+    while l1 || l2 || carry > 0
+      sum = (l1&.val || 0) + (l2&.val || 0) + carry
+      digit = sum % 10
+      carry = sum / 10
+      node = ListNode.new(digit)
 
-def sum_values(l1, l2, carry)
-  sum = (l1&.val || 0) + (l2&.val || 0) + carry
-  [sum % 10, sum / 10]
+      last_node.next = node if last_node
+      last_node = node
+      head ||= node
+
+      l1 = l1&.next
+      l2 = l2&.next
+    end
+    head
 end
 
 describe 'solution' do
-  context 'with carry' do
-    before do
-      @l1 = ListNode.new(1)
-      @l2 = ListNode.new(9)
-    end
-
-    it { expect(add_two_numbers(@l1, @l2).val).to eq 0 }
-    it { expect(add_two_numbers(@l1, @l2).next.val).to eq 1 }
+  it 'adds two single digit nums without carry' do
+    l1 = ListNode.new(1)
+    l2 = ListNode.new(3)
+    expect(add_two_numbers(l1, l2).val).to eq 4
   end
 
-  context 'without carry' do
-    before do
-      @l1 = ListNode.new(1)
-      @l2 = ListNode.new(8)
-    end
-
-    it { expect(add_two_numbers(@l1, @l2).val).to eq 9 }
-    it { expect(add_two_numbers(@l1, @l2).next).to eq nil }
+  it 'adds two single digit nums with carry' do
+    l1 = ListNode.new(3)
+    l2 = ListNode.new(9)
+    expect(add_two_numbers(l1, l2).val).to eq 2
+    expect(add_two_numbers(l1, l2).next.val).to eq 1
   end
 
-  context 'different sizes' do
-    before do
-      @l1 = ListNode.new(1)
-      @l2 = ListNode.new(8, ListNode.new(7))
-    end
-
-    it { expect(add_two_numbers(@l1, @l2).val).to eq 9 }
-    it { expect(add_two_numbers(@l1, @l2).next.val).to eq 7 }
+  it 'adds two nums with carry' do
+    l1 = ListNode.new(3)
+    l2 = ListNode.new(9, ListNode.new(9))
+    expect(add_two_numbers(l1, l2).val).to eq 2
+    expect(add_two_numbers(l1, l2).next.val).to eq 0
+    expect(add_two_numbers(l1, l2).next.next.val).to eq 1
   end
 
-  context 'problem example' do
-    before do
-      @l1 = ListNode.new(2, ListNode.new(4, ListNode.new(3)))
-      @l2 = ListNode.new(5, ListNode.new(6, ListNode.new(4)))
-    end
-
-    it { expect(add_two_numbers(@l1, @l2).val).to eq 7 }
-    it { expect(add_two_numbers(@l1, @l2).next.val).to eq 0 }
-    it { expect(add_two_numbers(@l1, @l2).next.next.val).to eq 8 }
+  it 'adds two complex nums with carry' do
+    l1 = ListNode.new(9, ListNode.new(1, ListNode.new(2, ListNode.new(3, ListNode.new(8, ListNode.new(2))))))
+    l2 = ListNode.new(8, ListNode.new(3, ListNode.new(7, ListNode.new(2))))
+    expect(add_two_numbers(l1, l2).val).to eq 7
+    expect(add_two_numbers(l1, l2).next.val).to eq 5
+    expect(add_two_numbers(l1, l2).next.next.val).to eq 9
+    expect(add_two_numbers(l1, l2).next.next.next.val).to eq 5
+    expect(add_two_numbers(l1, l2).next.next.next.next.val).to eq 8
+    expect(add_two_numbers(l1, l2).next.next.next.next.next.val).to eq 2
   end
 end
