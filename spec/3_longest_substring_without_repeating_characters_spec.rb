598ea5ee4ac3ad6ad658108602cc9194ab1eaceb
Paulo Soares <phsoares.ita@gmail.com>
2021-04-18 16:50:41 -0300

    longest substring

--- spec/3_longest_substring_without_repeating_characters_spec.rb
+++ spec/3_longest_substring_without_repeating_characters_spec.rb
@@ -1,48 +1,60 @@
 require 'spec_helper'
 require 'byebug'
 
-# @param {String} s
-# @return {Integer}
+# s.length <= 5 * 104
 def length_of_longest_substring(str)
   return str.length if str.length < 2
 
   first_pos = 0
   max_size = 0
-  map_of_positions = {}
+  chars_by_index = {}
   str.chars.each_with_index do |char, index|
-    if map_of_positions[char] && map_of_positions[char] >= first_pos
-      repeated_char_index = map_of_positions[char]
-      current_length = index - first_pos
-      first_pos = repeated_char_index + 1
-      max_size = [current_length, max_size].max
+    if chars_by_index[char]
+      max_size = [max_size, index - first_pos].max
+      first_pos = chars_by_index[char] + 1
+    else
+      max_size = [max_size, index - first_pos + 1].max
     end
-    map_of_positions[char] = index
+    chars_by_index[char] = index
   end
-  [str.length - first_pos, max_size].max
+  max_size
+end
+
+def map_chars_by_index(str)
+  ret = {}
+  str.chars.each_with_index do |char, index|
+    ret[char] ||= []
+    ret[char] << index
+  end
+  ret
 end
 
 describe 'solution' do
   it { expect(length_of_longest_substring('')).to eq 0 }
-  it { expect(length_of_longest_substring('1')).to eq 1 }
-  it { expect(length_of_longest_substring('11')).to eq 1 }
-  it { expect(length_of_longest_substring('12')).to eq 2 }
-  it { expect(length_of_longest_substring('121')).to eq 2 }
-  it { expect(length_of_longest_substring('122')).to eq 2 }
-  it { expect(length_of_longest_substring('123')).to eq 3 }
-  it { expect(length_of_longest_substring('1234')).to eq 4 }
-  it { expect(length_of_longest_substring('1134')).to eq 3 }
-  it { expect(length_of_longest_substring('1214')).to eq 3 }
-  it { expect(length_of_longest_substring('1231')).to eq 3 }
-  it { expect(length_of_longest_substring('12345')).to eq 5 }
-  it { expect(length_of_longest_substring('11345')).to eq 4 }
-  it { expect(length_of_longest_substring('12145')).to eq 4 }
-  it { expect(length_of_longest_substring('12315')).to eq 4 }
-  it { expect(length_of_longest_substring('12341')).to eq 4 }
-  it { expect(length_of_longest_substring('21341')).to eq 4 }
-  it { expect(length_of_longest_substring('21314')).to eq 3 }
-  it { expect(length_of_longest_substring('123427')).to eq 4 }
-  it { expect(length_of_longest_substring('123427891')).to eq 7 }
-  it { expect(length_of_longest_substring('dvdf')).to eq 3 }
-end
 
-# s.length <= 5 * 10**4
+  it { expect(length_of_longest_substring('1')).to eq 1 }
+
+  it { expect(length_of_longest_substring('12')).to eq 2 }
+  it { expect(length_of_longest_substring('11')).to eq 1 }
+
+  it { expect(length_of_longest_substring('123')).to eq 3 }
+  it { expect(length_of_longest_substring('112')).to eq 2 }
+  it { expect(length_of_longest_substring('211')).to eq 2 }
+  it { expect(length_of_longest_substring('111')).to eq 1 }
+
+  it { expect(length_of_longest_substring('1234')).to eq 4 }
+  it { expect(length_of_longest_substring('1124')).to eq 3 }
+  it { expect(length_of_longest_substring('2114')).to eq 2 }
+  it { expect(length_of_longest_substring('1114')).to eq 2 }
+  it { expect(length_of_longest_substring('1231')).to eq 3 }
+  it { expect(length_of_longest_substring('1121')).to eq 2 }
+  it { expect(length_of_longest_substring('2111')).to eq 2 }
+  it { expect(length_of_longest_substring('1111')).to eq 1 }
+
+  it { expect(length_of_longest_substring('0123456789')).to eq 10 }
+  it { expect(length_of_longest_substring('0123456780')).to eq 9 }
+  it { expect(length_of_longest_substring('0023456789')).to eq 9 }
+  it { expect(length_of_longest_substring('0103456789')).to eq 9 }
+  it { expect(length_of_longest_substring('1003456789')).to eq 8 }
+  it { expect(length_of_longest_substring('1023456780')).to eq 9 }
+end
