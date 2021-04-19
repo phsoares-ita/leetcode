4e4cebcd477f45879b3eddf44240dfdf88d141f8
Paulo Soares <phsoares.ita@gmail.com>
2021-04-18 21:51:19 -0300

    longest substring

--- spec/146_lru_cache_spec.rb
+++ spec/146_lru_cache_spec.rb
@@ -1,181 +1,85 @@
 require 'spec_helper'
 require 'byebug'
 
-class UniqueQueue
-  def initialize(capacity)
-    @capacity = capacity
-    @queue = []
-    @keys = {}
-  end
-
-  def add(key)
-    key_to_remove = nil
-    if present?(key)
-      remove(key)
-    elsif is_full?
-      key_to_remove = @queue.first
-      remove_first_key
-    end
-    @queue << key
-    @keys[key] = true
-    key_to_remove
-  end
-
-  private
-
-  def present?(key)
-    @keys[key]
-  end
-
-  def is_full?
-    @queue.length == @capacity
-  end
-
-  def remove_first_key
-    first_key = @queue.shift
-    @keys.delete(first_key)
-  end
-
-  def remove(key)
-    @queue.delete(key)
-    @keys.delete(key)
-  end
-end
-
-describe UniqueQueue do
-  context 'below capacity queue' do
-    it 'should indicate keys to remove' do
-      @queue = UniqueQueue.new(2)
-
-      expect(@queue.add(1)).to eq(nil)
-      expect(@queue.add(2)).to eq(nil)
-      expect(@queue.add(3)).to eq(1)
-      expect(@queue.add(3)).to eq(nil)
-      expect(@queue.add(4)).to eq(2)
-      expect(@queue.add(3)).to eq(nil)
-      expect(@queue.add(4)).to eq(nil)
-    end
-  end
-end
-
 class LRUCache
   def initialize(capacity)
-    @queue = UniqueQueue.new(capacity)
-    @items = {}
+    @capacity = capacity
+    @map = {}
   end
 
   # Return the value of the key if the key exists, otherwise return -1.
   def get(key)
-    return -1 unless @items[key]
-
-    @queue.add(key)
-    @items[key]
+    if @map[key]
+      refresh_cache(key)
+      @map[key]
+    else
+      -1
+    end
   end
 
   # Update the value of the key if the key exists.
   # Otherwise, add the key-value pair to the cache.
   # If the number of keys exceeds the capacity from this operation, evict the least recently used key.
   def put(key, value)
-    key_to_remove = @queue.add(key)
-    @items.delete(key_to_remove) if key_to_remove
-    @items[key] = value
+    key_to_remove = refresh_cache(key)
+    @map.delete(key_to_remove) if key_to_remove
+    @map[key] = value
+  end
+
+  private
+
+  def refresh_cache(key)
+    @use_record ||= []
+    if @use_record.include?(key)
+      key_to_remove = nil
+      @use_record.delete(key)
+    elsif @use_record.length == @capacity
+      key_to_remove = @use_record.shift
+    end
+    @use_record << key
+    key_to_remove
   end
 end
 
 describe 'solution' do
-  context 'empty cache' do
-    before do
-      @cache = LRUCache.new(3)
-    end
+  it 'works as map until capacity is reached' do
+    cache = LRUCache.new(2)
+    expect(cache.get(1)).to eq(-1)
+    expect(cache.get(2)).to eq(-1)
 
-    it { expect(@cache.get(0)).to eq -1 }
+    cache.put(1, 10)
+    expect(cache.get(1)).to eq(10)
+    expect(cache.get(2)).to eq(-1)
+
+    cache.put(2, 11)
+    expect(cache.get(1)).to eq(10)
+    expect(cache.get(2)).to eq(11)
   end
 
-  context 'below capacity cache' do
-    before do
-      @cache = LRUCache.new(3)
-      @cache.put(0, 10)
-    end
+  it 'drops least recently added key if none is accessed' do
+    cache = LRUCache.new(2)
+    cache.put(1, 10)
+    cache.put(2, 11)
+    cache.put(3, 12)
 
-    it { expect(@cache.get(0)).to eq(10) }
-    it { expect(@cache.get(1)).to eq(-1) }
+    expect(cache.get(1)).to eq(-1)
+    expect(cache.get(2)).to eq(11)
+    expect(cache.get(3)).to eq(12)
   end
 
-  context 'equal capacity cache' do
-    before do
-      @cache = LRUCache.new(3)
-      @cache.put(0, 10)
-      @cache.put(1, 11)
-      @cache.put(2, 12)
-    end
+  it 'drops least recently added key if some key is accessed' do
+    cache = LRUCache.new(2)
+    cache.put(1, 10)
+    cache.put(2, 11)
+    cache.get(1)
+    cache.get(1)
+    cache.get(1)
+    cache.get(1)
+    cache.get(1)
+    cache.put(3, 12)
 
-    it { expect(@cache.get(0)).to eq(10) }
-    it { expect(@cache.get(1)).to eq(11) }
-    it { expect(@cache.get(2)).to eq(12) }
-    it { expect(@cache.get(3)).to eq(-1) }
-  end
-
-  context 'multiple gets' do
-    before do
-      @cache = LRUCache.new(3)
-      @cache.put(0, 10)
-      @cache.put(1, 11)
-      @cache.put(2, 12)
-
-      @cache.get(1)
-      @cache.get(1)
-      @cache.get(1)
-      @cache.get(1)
-      @cache.get(1)
-
-      @cache.put(3, 13)
-    end
-
-    it { expect(@cache.get(0)).to eq(-1) }
-    it { expect(@cache.get(2)).to eq(12) }
-  end
-
-  context 'exceding capacity cache' do
-    before do
-      @cache = LRUCache.new(3)
-      @cache.put(0, 10)
-      @cache.put(1, 11)
-      @cache.put(2, 12)
-      @cache.put(3, 13)
-    end
-
-    it { expect(@cache.get(0)).to eq(-1) }
-    it { expect(@cache.get(1)).to eq(11) }
-    it { expect(@cache.get(2)).to eq(12) }
-    it { expect(@cache.get(3)).to eq(13) }
-
-    context 'getting old key should refresh cache usage' do
-      it 'getting 1 should make it not expire' do
-        @cache.get(1)
-        @cache.put(4, 14)
-        expect(@cache.get(1)).to eq(11)
-        expect(@cache.get(2)).to eq(-1)
-      end
-    end
-
-    context 'problem example' do
-      before do
-        @cache = LRUCache.new(2)
-      end
-
-      it 'works as intended' do
-        @cache.put(1, 1)
-        @cache.put(2, 2)
-        expect(@cache.get(1)).to eq(1)
-
-        @cache.put(3, 3)
-        expect(@cache.get(2)).to eq(-1)
-
-        @cache.put(4, 4)
-        expect(@cache.get(1)).to eq(-1)
-        expect(@cache.get(3)).to eq(3)
-        expect(@cache.get(4)).to eq(4)
-      end
-    end
+    expect(cache.get(1)).to eq(10)
+    expect(cache.get(2)).to eq(-1)
+    expect(cache.get(3)).to eq(12)
   end
 end
--- spec/3_longest_substring_without_repeating_characters_spec.rb
+++ spec/3_longest_substring_without_repeating_characters_spec.rb
@@ -1,60 +1,48 @@
 require 'spec_helper'
 require 'byebug'
 
-# s.length <= 5 * 104
 def length_of_longest_substring(str)
-  return str.length if str.length < 2
-
-  first_pos = 0
-  max_size = 0
-  chars_by_index = {}
-  str.chars.each_with_index do |char, index|
-    if chars_by_index[char]
-      max_size = [max_size, index - first_pos].max
-      first_pos = chars_by_index[char] + 1
+  pos1 = 0
+  pos2 = 0
+  ret = 0
+  char_indices = {}
+  while pos2 < str.length
+    char = str[pos2]
+    if char_indices[char]
+      ret = [ret, pos2 - pos1].max
+      pos1 = char_indices[char] + 1
+      char_indices = char_indices.select { |_, v| v >= pos1 }
     else
-      max_size = [max_size, index - first_pos + 1].max
+      ret = [ret, pos2 - pos1 + 1].max
     end
-    chars_by_index[char] = index
-  end
-  max_size
-end
-
-def map_chars_by_index(str)
-  ret = {}
-  str.chars.each_with_index do |char, index|
-    ret[char] ||= []
-    ret[char] << index
+    char_indices[char] = pos2
+    pos2 += 1
   end
   ret
 end
 
 describe 'solution' do
-  it { expect(length_of_longest_substring('')).to eq 0 }
+  context 'all different chars' do
+    it { expect(length_of_longest_substring('')).to eq(0) }
+    it { expect(length_of_longest_substring('1')).to eq(1) }
+    it { expect(length_of_longest_substring('12')).to eq(2) }
+    it { expect(length_of_longest_substring('12345')).to eq(5) }
+  end
 
-  it { expect(length_of_longest_substring('1')).to eq 1 }
+  context 'two equal chars' do
+    it { expect(length_of_longest_substring('11')).to eq(1) }
+    it { expect(length_of_longest_substring('011')).to eq(2) }
+    it { expect(length_of_longest_substring('101')).to eq(2) }
+    it { expect(length_of_longest_substring('110')).to eq(2) }
+    it { expect(length_of_longest_substring('11ABCD')).to eq(5) }
+    it { expect(length_of_longest_substring('A11ABCD')).to eq(5) }
+    it { expect(length_of_longest_substring('1BCD1A')).to eq(5) }
+    it { expect(length_of_longest_substring('BCD11A')).to eq(4) }
+  end
 
-  it { expect(length_of_longest_substring('12')).to eq 2 }
-  it { expect(length_of_longest_substring('11')).to eq 1 }
-
-  it { expect(length_of_longest_substring('123')).to eq 3 }
-  it { expect(length_of_longest_substring('112')).to eq 2 }
-  it { expect(length_of_longest_substring('211')).to eq 2 }
-  it { expect(length_of_longest_substring('111')).to eq 1 }
-
-  it { expect(length_of_longest_substring('1234')).to eq 4 }
-  it { expect(length_of_longest_substring('1124')).to eq 3 }
-  it { expect(length_of_longest_substring('2114')).to eq 2 }
-  it { expect(length_of_longest_substring('1114')).to eq 2 }
-  it { expect(length_of_longest_substring('1231')).to eq 3 }
-  it { expect(length_of_longest_substring('1121')).to eq 2 }
-  it { expect(length_of_longest_substring('2111')).to eq 2 }
-  it { expect(length_of_longest_substring('1111')).to eq 1 }
-
-  it { expect(length_of_longest_substring('0123456789')).to eq 10 }
-  it { expect(length_of_longest_substring('0123456780')).to eq 9 }
-  it { expect(length_of_longest_substring('0023456789')).to eq 9 }
-  it { expect(length_of_longest_substring('0103456789')).to eq 9 }
-  it { expect(length_of_longest_substring('1003456789')).to eq 8 }
-  it { expect(length_of_longest_substring('1023456780')).to eq 9 }
+  context 'three equal chars' do
+    it { expect(length_of_longest_substring('111')).to eq(1) }
+    it { expect(length_of_longest_substring('0111')).to eq(2) }
+    it { expect(length_of_longest_substring('ABCD111')).to eq(5) }
+  end
 end
