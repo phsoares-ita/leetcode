5467a112057a6fe68d8599d972ab863271ec23ba
Paulo Soares <phsoares.ita@gmail.com>
2021-05-08 15:04:02 -0300

    ransom note

--- 383_ransom_note/spec/solution_spec.rb
+++ 383_ransom_note/spec/solution_spec.rb
@@ -1,8 +1,30 @@
 require 'spec_helper'
 require 'byebug'
 
-def solution(nums1); end
+# @param {String} ransom_note
+# @param {String} magazine
+# @return {Boolean}
+def can_construct(ransom_note, magazine)
+  occurrences = map_occurrences(magazine)
+  ransom_note.chars.each do |char|
+    next if char =~ /\s/
+    return false if occurrences[char].zero?
+
+    occurrences[char] -= 1
+  end
+  true
+end
+
+def map_occurrences(text)
+  ret = {}
+  text.chars.each do |char|
+    ret[char] ||= 0
+    ret[char] += 1
+  end
+  ret
+end
 
 describe 'solution' do
-  it { expect(solution([1])).to eq 1 }
+  it { expect(can_construct('i love you', 'iloveyou')).to eq true }
+  it { expect(can_construct('i love you', 'iloveya')).to eq false }
 end
