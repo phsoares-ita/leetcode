1eb47be0b89236a3c89a51f09446fddf45a9e9c8
Paulo Soares <phsoares.ita@gmail.com>
2021-05-10 02:17:58 -0300

    excel sheet column

--- solutions/168_excel_sheet_column_title/spec/solution_spec.rb
+++ solutions/168_excel_sheet_column_title/spec/solution_spec.rb
@@ -1,8 +1,23 @@
 require 'spec_helper'
 require 'byebug'
 
-def solution(num); end
+# @param {Integer} column_number
+# @return {String}
+def convert_to_title(x)
+  letters = ('A'..'Z').to_a
+  if (x - 1) / 26 == 0
+    letters[x - 1]
+  elsif (x - 1) / 26 == 1
+    'A' + letters[(x - 1) % 26]
+  elsif (x - 1) / 26 == 2
+    'B' + letters[(x - 1) % 26]
+  end
+end
 
 describe 'solution' do
-  it { expect(solution(1)).to eq 1 }
+  it { expect(convert_to_title(1)).to eq 'A' }
+  it { expect(convert_to_title(26)).to eq 'Z' }
+  it { expect(convert_to_title(27)).to eq 'AA' }
+  it { expect(convert_to_title(52)).to eq 'AZ' }
+  it { expect(convert_to_title(53)).to eq 'BA' }
 end
